0bf07133a108bdfa4a54ee1d1e355e56dbdbefc2
مؤمن <71411025+Moumin01@users.noreply.github.com>
2022-11-17 23:16:06 +0100

    small info

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const { json } = require('express')
 const express = require('express')
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let info = {
+    hadith: "/hadith/:book/:number",
+    search: "/search/:search",
+    random: "/hadith/random",
+    book: "/hadith/:book"
+  };
+  res.send({info})
 })
 
 app.listen(port, () => {
